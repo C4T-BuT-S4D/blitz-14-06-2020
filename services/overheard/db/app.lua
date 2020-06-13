6e3a056447c1eb0c30ab4e85ef7c5f9191c6544c
jnovikov <johnnovikov0@gmail.com>
2020-06-13 23:11:53 +0300

    fix random issue

--- services/overheard/db/app.lua
+++ services/overheard/db/app.lua
@@ -2,14 +2,9 @@
 -- Configure database
 
 clock = require('clock')
+uuid = require('uuid')
 
-function string.random(length)
-  charset = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
-  math.randomseed(os.time())
-  if length > 0 then return string.random(length - 1) .. charset[math.random(1, #charset)] else return "" end
-end
-
-key = string.random(15)
+key = uuid()
 
 box.cfg {
    listen = 3301
